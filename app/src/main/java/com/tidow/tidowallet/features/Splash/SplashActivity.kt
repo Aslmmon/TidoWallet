e353fc1ac0697ae13cb23027e8e2c79cf65edf2d
aslmm <aslmmon993@gmail.com>
2021-05-21 11:14:45 +0200

    - added Splash and Login Activities as Empty Activities

--- app/src/main/java/com/tidow/tidowallet/features/Splash/SplashActivity.kt
+++ app/src/main/java/com/tidow/tidowallet/features/Splash/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.tidow.tidowallet
+package com.tidow.tidowallet.features.Splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tidow.tidowallet.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
