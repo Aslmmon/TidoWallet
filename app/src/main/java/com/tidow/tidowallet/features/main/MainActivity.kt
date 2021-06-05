5e909fac2da64c10371e0eae4e67d28859d6e8be
aslmm <aslmmon993@gmail.com>
2021-06-05 21:38:33 +0200

    - add bottom navigation in Home Activity ..

--- app/src/main/java/com/tidow/tidowallet/features/main/MainActivity.kt
+++ app/src/main/java/com/tidow/tidowallet/features/main/MainActivity.kt
@@ -2,6 +2,10 @@ package com.tidow.tidowallet.features.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.button.MaterialButton
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -9,14 +13,18 @@ import com.tidow.tidowallet.R
 import com.tidow.tidowallet.custom.Navigation
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val btn = findViewById<MaterialButton>(R.id.btn_signOut)
-        btn.setOnClickListener {
-            Firebase.auth.signOut()
-            Navigation.goToLoginActivity(this)
-        }
+        bottomNav = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(bottomNav, navController)
+//
+//        val btn = findViewById<MaterialButton>(R.id.btn_signOut)
+//        btn.setOnClickListener {
+//            Firebase.auth.signOut()
+//            Navigation.goToLoginActivity(this)
+//        }
     }
 }
---
+++ app/src/main/java/com/tidow/tidowallet/features/main/fragments/QrCodeFragment.kt
@@ -0,0 +1,26 @@
+package com.tidow.tidowallet.features.main.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tidow.tidowallet.R
+
+
+class QrCodeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_qr_code, container, false)
+    }
+
+}
